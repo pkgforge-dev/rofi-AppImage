a2e181a381355ff76d7cc4b70d8e7862c2ef4ca0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-03 18:49:45 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+set -ex
+ARCH="$(uname -m)"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+
+case "$ARCH" in
+	'x86_64')  PKG_TYPE='x86_64.pkg.tar.zst';;
+	'aarch64') PKG_TYPE='aarch64.pkg.tar.xz';;
+	''|*) echo "Unknown arch: $ARCH"; exit 1;;
+esac
+
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+
 pacman -Syu --noconfirm \
 	base-devel           \
 	bison                \
@@ -25,3 +39,14 @@ pacman -Syu --noconfirm \
 	xcb-util-wm          \
 	xcb-util-xrm         \
 	xkeyboard-config
+
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2-iculess.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+echo "All done!"
+echo "---------------------------------------------------------------"
