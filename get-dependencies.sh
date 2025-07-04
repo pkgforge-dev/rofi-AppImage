a34fdfcbe047e7b8312ef6378d7a7c340c61560a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-04 00:31:10 +0000

    build on archlinux, add `aarch64` and appbundles, switch to dwarfs (#15)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,31 +1,53 @@
 #!/bin/sh
 
-apk add \
-	bash                     \
-	bison                    \
-	build-base               \
-	cairo-dev                \
-	check-dev                \
-	coreutils                \
-	cppcheck                 \
-	flex                     \
-	gdk-pixbuf-dev           \
-	git                      \
-	glib-dev                 \
-	gvfs                     \
-	librsvg-dev              \
-	libxcb-dev               \
-	libxkbcommon-dev         \
-	meson                    \
-	musl-fts-dev             \
-	pango-dev                \
-	ronn                     \
-	startup-notification-dev \
-	strace                   \
-	wget                     \
-	xcb-util-cursor-dev      \
-	xcb-util-dev             \
-	xcb-util-keysyms-dev     \
-	xcb-util-wm-dev          \
-	xcb-util-xrm-dev         \
-	xkeyboard-config-dev
+set -ex
+ARCH="$(uname -m)"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+
+case "$ARCH" in
+	'x86_64')  PKG_TYPE='x86_64.pkg.tar.zst';;
+	'aarch64') PKG_TYPE='aarch64.pkg.tar.xz';;
+	''|*) echo "Unknown arch: $ARCH"; exit 1;;
+esac
+
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+
+pacman -Syu --noconfirm \
+	base-devel           \
+	bison                \
+	cairo                \
+	check                \
+	cppcheck             \
+	flex                 \
+	gdk-pixbuf2          \
+	git                  \
+	gvfs                 \
+	librsvg              \
+	libxcb               \
+	libxkbcommon         \
+	libxkbcommon-x11     \
+	meson                \
+	pango                \
+	patch                \
+	startup-notification \
+	wget                 \
+	xcb-util-cursor      \
+	xcb-util             \
+	xcb-util-keysyms     \
+	xcb-util-wm          \
+	xcb-util-xrm         \
+	xkeyboard-config     \
+	zsync
+
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2-iculess.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+echo "All done!"
+echo "---------------------------------------------------------------"
