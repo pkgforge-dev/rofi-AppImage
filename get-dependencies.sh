f72c6b2d392f98ac14ce150c7988f154f4bd6fc5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-24 12:36:14 +0000

    move to `quick-sharun` (#18)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,6 +2,7 @@
 
 set -ex
 EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+PATCH="$PWD"/locale-fix.patch
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
@@ -37,3 +38,14 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh libxml2-mini gtk3-mini
+
+echo "Building rofi..."
+echo "---------------------------------------------------------------"
+git clone --depth 1 "https://github.com/davatorium/rofi.git" ./rofi && (
+	cd ./rofi
+	patch -p1 -i "$PATCH"
+	meson --prefix /usr . build
+	meson compile -C build
+	meson install -C build
+)
+/usr/bin/rofi -v | awk -F'[- ]' '{print $2; exit}' > ~/version
