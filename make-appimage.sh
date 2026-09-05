0b7990e25533ee6885817bb3f4082ee7f5b99b2c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-04 20:19:00 -0400

    update template and hook (#33)
    * Update README.md
    
    * Update make-appimage.sh
    
    ---------
    
    Co-authored-by: Link4Electronics <link4electronics@gmail.com>

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/rofi.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/rofi.svg
